24b201683fea36584c3c23bbec4b069b5dafa31d
Leo Winkler <lwinkler@student.tgm.ac.at>
2017-03-13 13:48:04 +0100

    1. Version

---
+++ src/winkler/Gruessen1.java
@@ -0,0 +1,17 @@
+package winkler;
+
+public class Gruessen1 {
+
+	public static void gutenTag(){
+		
+		System.out.println("GutenTag.");
+	}
+	
+	public static void aufWiedersehen(){
+		
+		System.out.println("AufWiedersehen.");
+	}
+	
+	public static void main(String[]args){}
+	
+}
