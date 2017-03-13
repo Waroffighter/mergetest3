2d038b0e5088d86ed0b1602cb99358f848d555b0
Leo Winkler <lwinkler@student.tgm.ac.at>
2017-03-13 14:08:30 +0100

    Mit Smileys

--- src/winkler/Gruessen1.java
+++ src/winkler/Gruessen1.java
@@ -4,12 +4,12 @@ public class Gruessen1 {
 
 	public static void gutenTag(){
 		
-		System.out.println("GutenTag.");
+		System.out.println("GutenTag:):):).");
 	}
 	
 	public static void aufWiedersehen(){
 		
-		System.out.println("AufWiedersehen.");
+		System.out.println("AufWiedersehen./////");
 	}
 	
 	public static void main(String[]args){}
