22482a3c28f11d5afb1af92700c4bd27ed76be6c
Leo Winkler <lwinkler@student.tgm.ac.at>
2017-03-26 15:01:33 +0200

    neuer Text

--- src/winkler/Gruessen1.java
+++ src/winkler/Gruessen1.java
@@ -12,6 +12,8 @@ public class Gruessen1 {
 		
 		System.out.println("AufWiedersehen.!!!!");
 		System.out.println("AufWiedersehen./////");
+		System.out.println("miep");
+	
 	}
 	
 	public static void main(String[]args){}
